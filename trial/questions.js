ffc3d9ce8490799799366aa1127603bcb126e3f3
root <root@localhost>
2011-12-22 13:51:12 +0000

    Start of data collection forms for user trials

---
+++ trial/questions.js
@@ -0,0 +1,44 @@
+
+	//current screen
+	var screen = 0;
+	
+	var start = function() {
+		//$.Storage.set("screen1","1,2,3,4,5,6,7,8,9");
+		//alert($.Storage.get("screen1"));
+		screen = getScreen();
+	};
+	
+	var save = function() {
+	   
+	   var data = [];
+	   
+	   $(".answer").each(function(i) {
+	   		var store = '';
+	   		$("input",this).each(function(j) {
+	   			if(this.checked) {
+	   				store = this.value;
+	   			}
+	   		});
+	   		
+	   		data.push(store)
+	   });
+	   
+	   $.Storage.set("screen" + screen,data.toString());
+	   
+	   screen = getScreen(screen + 1);
+	};
+	
+	var getScreen = function (screenNo) {
+		if(screenNo == null) {
+			screenNo = 1;
+		}
+		url = "./screens/screen" + screenNo + ".html";
+		$("#form").load(url);
+		
+		$('html, body').animate({scrollTop:0}, 'slow');
+		
+		return screenNo;
+	};
+	
+	
+	
