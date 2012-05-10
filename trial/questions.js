e16da8362c7f1a5a7b9a075837789289888adb37
root <root@localhost>
2012-05-10 08:00:27 +0000

    data logging fixes

--- trial/questions.js
+++ trial/questions.js
@@ -4,7 +4,8 @@
 	
 	var start = function() {
 		//$.Storage.set("screen1","1,2,3,4,5,6,7,8,9");
-		//alert($.Storage.get("screen1"));
+		alert($.Storage.get("screen1"));
+		alert($.Storage.get("screen2"));
 		screen = getScreen();
 	};
 	
@@ -12,11 +13,32 @@
 	   
 	   var data = [];
 	   
+	   data.push(getPid());
+	   
 	   $(".answer").each(function(i) {
 	   		var store = '';
-	   		$("input",this).each(function(j) {
-	   			if(this.checked) {
-	   				store = this.value;
+	   		$("input,select,textarea",this).each(function(j) {
+	   			
+	   			switch(this.type) {
+	   			  case "text":
+	   			    store = this.value;
+	   			  break;
+	   			  case "radio":
+	   			    if(this.checked) {
+	   				  store = this.value;
+	   			    }
+	   			  break;
+	   			  case "checkbox":
+	   			    if(this.checked) {
+	   			  	  store = this.value;
+	   			    }
+	   			  break;
+	   			  case "select-one":
+	   			  	store = this.value;
+	   			  break;
+	   			  case "textarea":
+	   			  	store = this.value;
+	   			  break;
 	   			}
 	   		});
 	   		
@@ -24,6 +46,9 @@
 	   });
 	   
 	   $.Storage.set("screen" + screen,data.toString());
+	   
+	   //save the screen
+	   
 	   
 	   screen = getScreen(screen + 1);
 	};
@@ -40,5 +65,12 @@
 		return screenNo;
 	};
 	
+	pid = "";
+	var getPid = function() {
+	  if(pid == "") {
+	  	pid = new Date().getTime();
+	  }
+	  return pid;
+	}
 	
 	
